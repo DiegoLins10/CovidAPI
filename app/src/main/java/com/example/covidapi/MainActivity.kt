88242e4f2794c849eecf1d16998d64d2889e65cb
Diego Lins <68483773+DiegoLins10@users.noreply.github.com>
2021-05-24 21:09:28 -0300

    api covid 24/05/21

---
+++ app/src/main/java/com/example/covidapi/MainActivity.kt
@@ -0,0 +1,90 @@
+package com.example.covidapi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.casos.*
+import kotlinx.android.synthetic.main.casos_hoje.*
+import kotlinx.android.synthetic.main.imagem.*
+import kotlinx.android.synthetic.main.mortes.*
+import kotlinx.android.synthetic.main.populacao.*
+import kotlinx.android.synthetic.main.recuperados.*
+import kotlinx.android.synthetic.main.recuperados_hoje.*
+import org.jetbrains.anko.alert
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
+import org.json.JSONObject
+import java.net.URL
+import java.text.NumberFormat
+import java.text.SimpleDateFormat
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    val API_URL = "https://disease.sh/v3/covid-19/countries/brazil?strict=true"
+
+    var totalCasos: Long = 0
+    var totalMortes: Long = 0
+    var populacao: Long = 0
+    var totalCasosHoje: Long = 0
+    var totalRecuperados: Long = 0
+    var recuperadosHoje: Long = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        buscaDados()
+        /*btnCalcular.setOnClickListener(){
+            calcular()
+        }*/
+    }
+    fun buscaDados(){
+        doAsync {
+            //acessar API e buscar seu resultado
+            val resposta = URL(API_URL).readText()
+            totalCasos = JSONObject(resposta).getLong("cases")
+            totalMortes = JSONObject(resposta).getLong("deaths")
+            populacao = JSONObject(resposta).getLong("population")
+            totalCasosHoje = JSONObject(resposta).getLong("todayCases")
+            totalRecuperados = JSONObject(resposta).getLong("recovered")
+            recuperadosHoje = JSONObject(resposta).getLong("todayRecovered")
+            val f = NumberFormat.getNumberInstance(Locale("pt", "br"))
+            val date: Date = Date()
+            val formato = "dd/MM/yyyy HH:mm:ss"
+            val format = SimpleDateFormat(formato)
+            val dataFormatada = format.format(date)
+
+            val totalCasosFormatado = f.format(totalCasos)
+            val totalMortesFormatada = f.format(totalMortes)
+            val populacaoFormatada = f.format(populacao)
+            val totalCasosHojeFormatada = f.format(totalCasosHoje)
+            val totalRecuperadosFormatada = f.format(totalRecuperados)
+            val recuperadosHojeFormatada = f.format(recuperadosHoje)
+
+            uiThread {
+                // alert ("$cotacaoBitcoin").show()
+                //alert("$totalCasosFormatado").show()
+                txtCasos.setText("$totalCasosFormatado   $dataFormatada")
+                txtTotalMortes.setText("$totalMortesFormatada")
+                txtPopulacao.setText("$populacaoFormatada")
+                txtCasosHoje.setText("$totalCasosHojeFormatada")
+                txtRecuperados.setText("$totalRecuperadosFormatada")
+                txtRecuperadosHoje.setText("$recuperadosHojeFormatada")
+
+            }
+        }
+    }
+   /* fun calcular(){
+        if(txtValor.text.isEmpty()){
+            txtValor.error = "Preencha um valor"
+            return
+        }
+        //valor digitado usuario
+        val valorDigitado = txtValor.text.toString()
+            .replace(",", ".").toDouble()
+        //.replace(".","").toDouble().toDouble()
+        val resultado = if(cotacaoBitcoin>0) valorDigitado / cotacaoBitcoin
+        else 0.0
+        txtQtdBitcoins.text = "%.8f".format(resultado)
+    }*/
+}
